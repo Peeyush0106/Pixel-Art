1abbdab5f3c1013bc452f58a279878460b13bc83
Debug0106 <69502115+Peeyush0106@users.noreply.github.com>
2025-11-09 22:23:57 +0530

    final

--- code.js
+++ code.js
@@ -3,221 +3,341 @@
 var p5Inst = new p5(null, 'sketch');
 
 window.preload = function () {
-	p5Inst._predefinedSpriteAnimations = {};
-	p5Inst._pauseSpriteAnimationsByDefault = false;
-	var animationListJSON = { "orderedKeys": ["3998a111-b9b1-4366-857d-18da1217e47e", "e3cf9ad0-8cdb-49f8-ae12-2cca1ccc9a69"], "propsByKey": { "3998a111-b9b1-4366-857d-18da1217e47e": { "name": "title2", "sourceUrl": null, "frameSize": { "x": 661, "y": 607 }, "frameCount": 1, "looping": true, "frameDelay": 12, "version": "lzmzNMWZ6qSYQc2GlUNdBidKDsBXwfGL", "loadedFromSource": true, "saved": true, "sourceSize": { "x": 661, "y": 607 }, "rootRelativePath": "assets/3998a111-b9b1-4366-857d-18da1217e47e.png" }, "e3cf9ad0-8cdb-49f8-ae12-2cca1ccc9a69": { "name": "start1", "sourceUrl": "assets/v3/animations/PqOp0ou8kvykADg3G6mnIrv7XUwHPZUZD4rJQznU7OA/e3cf9ad0-8cdb-49f8-ae12-2cca1ccc9a69.png", "frameSize": { "x": 488, "y": 217 }, "frameCount": 1, "looping": true, "frameDelay": 4, "version": "fJXno2H8dd7Kc8kX0Lg6xSs9jJIKv_i8", "loadedFromSource": true, "saved": true, "sourceSize": { "x": 488, "y": 217 }, "rootRelativePath": "assets/v3/animations/PqOp0ou8kvykADg3G6mnIrv7XUwHPZUZD4rJQznU7OA/e3cf9ad0-8cdb-49f8-ae12-2cca1ccc9a69.png" } } };
-	var orderedKeys = animationListJSON.orderedKeys;
-	var allAnimationsSingleFrame = false;
-	orderedKeys.forEach(function (key) {
-		var props = animationListJSON.propsByKey[key];
-		var frameCount = allAnimationsSingleFrame ? 1 : props.frameCount;
-		var image = loadImage(props.rootRelativePath, function () {
-			var spriteSheet = loadSpriteSheet(
-				image,
-				props.frameSize.x,
-				props.frameSize.y,
-				frameCount
-			);
-			p5Inst._predefinedSpriteAnimations[props.name] = loadAnimation(spriteSheet);
-			p5Inst._predefinedSpriteAnimations[props.name].looping = props.looping;
-			p5Inst._predefinedSpriteAnimations[props.name].frameDelay = props.frameDelay;
-		});
-	});
+  p5Inst._predefinedSpriteAnimations = {};
+  p5Inst._pauseSpriteAnimationsByDefault = false;
+  var animationListJSON = { "orderedKeys": ["1db8aaa9-04bd-43b7-a968-b67b64481c3b", "7756880a-f1d7-4862-a55d-536ac98bec1e", "d0a9ed9c-26b9-4ec1-a286-f20123654c64", "3edc2d30-f8ff-4f59-87e6-5a49a49e6df9", "c8c253dd-0163-443b-8512-f0e9e377fc7a", "783aff6f-d51c-4b21-bd9b-8017fd2c11ac"], "propsByKey": { "1db8aaa9-04bd-43b7-a968-b67b64481c3b": { "name": "bg1.png", "sourceUrl": "assets/v3/animations/IPKnGiLe9d70KdB_tW1BNYeLq3oPdwB9TY0LPraPhR4/1db8aaa9-04bd-43b7-a968-b67b64481c3b.png", "frameSize": { "x": 402, "y": 403 }, "frameCount": 1, "looping": true, "frameDelay": 4, "version": "JQ1F1YGdpLoKzMFuaGCjFfXr80yrkEc7", "loadedFromSource": true, "saved": true, "sourceSize": { "x": 402, "y": 403 }, "rootRelativePath": "assets/v3/animations/IPKnGiLe9d70KdB_tW1BNYeLq3oPdwB9TY0LPraPhR4/1db8aaa9-04bd-43b7-a968-b67b64481c3b.png" }, "7756880a-f1d7-4862-a55d-536ac98bec1e": { "name": "bg2.png", "sourceUrl": "assets/v3/animations/IPKnGiLe9d70KdB_tW1BNYeLq3oPdwB9TY0LPraPhR4/7756880a-f1d7-4862-a55d-536ac98bec1e.png", "frameSize": { "x": 402, "y": 402 }, "frameCount": 1, "looping": true, "frameDelay": 4, "version": "ZQQ0fGcdao1VPMoYKJqp9LixRg9dqjzY", "loadedFromSource": true, "saved": true, "sourceSize": { "x": 402, "y": 402 }, "rootRelativePath": "assets/v3/animations/IPKnGiLe9d70KdB_tW1BNYeLq3oPdwB9TY0LPraPhR4/7756880a-f1d7-4862-a55d-536ac98bec1e.png" }, "d0a9ed9c-26b9-4ec1-a286-f20123654c64": { "name": "rover1.png", "sourceUrl": null, "frameSize": { "x": 103, "y": 75 }, "frameCount": 3, "looping": true, "frameDelay": 12, "version": "Qq6uoLbxGEb7qWwy6av2oi1zhE_Wf9sN", "loadedFromSource": true, "saved": true, "sourceSize": { "x": 103, "y": 225 }, "rootRelativePath": "assets/d0a9ed9c-26b9-4ec1-a286-f20123654c64.png" }, "3edc2d30-f8ff-4f59-87e6-5a49a49e6df9": { "name": "rover2.png", "sourceUrl": null, "frameSize": { "x": 84, "y": 87 }, "frameCount": 3, "looping": true, "frameDelay": 12, "version": "ZtLA5kuH5MkWwMGNLmkdiw8vlexVQtFV", "loadedFromSource": true, "saved": true, "sourceSize": { "x": 168, "y": 174 }, "rootRelativePath": "assets/3edc2d30-f8ff-4f59-87e6-5a49a49e6df9.png" }, "c8c253dd-0163-443b-8512-f0e9e377fc7a": { "name": "rover3.png", "sourceUrl": null, "frameSize": { "x": 113, "y": 102 }, "frameCount": 3, "looping": true, "frameDelay": 12, "version": "tCfqPvQ_xw3_1cpH3wGAKhzRkJSKFtUW", "loadedFromSource": true, "saved": true, "sourceSize": { "x": 226, "y": 204 }, "rootRelativePath": "assets/c8c253dd-0163-443b-8512-f0e9e377fc7a.png" }, "783aff6f-d51c-4b21-bd9b-8017fd2c11ac": { "name": "rover4.png", "sourceUrl": null, "frameSize": { "x": 145, "y": 75 }, "frameCount": 2, "looping": true, "frameDelay": 12, "version": "hogIKj7cCukAJOIatl1i9CZswwmguwf2", "loadedFromSource": true, "saved": true, "sourceSize": { "x": 145, "y": 150 }, "rootRelativePath": "assets/783aff6f-d51c-4b21-bd9b-8017fd2c11ac.png" } } };
+  var orderedKeys = animationListJSON.orderedKeys;
+  var allAnimationsSingleFrame = false;
+  orderedKeys.forEach(function (key) {
+    var props = animationListJSON.propsByKey[key];
+    var frameCount = allAnimationsSingleFrame ? 1 : props.frameCount;
+    var image = loadImage(props.rootRelativePath, function () {
+      var spriteSheet = loadSpriteSheet(
+        image,
+        props.frameSize.x,
+        props.frameSize.y,
+        frameCount
+      );
+      p5Inst._predefinedSpriteAnimations[props.name] = loadAnimation(spriteSheet);
+      p5Inst._predefinedSpriteAnimations[props.name].looping = props.looping;
+      p5Inst._predefinedSpriteAnimations[props.name].frameDelay = props.frameDelay;
+    });
+  });
 
-	function wrappedExportedCode(stage) {
-		if (stage === 'preload') {
-			if (setup !== window.setup) {
-				window.setup = setup;
-			} else {
-				return;
-			}
-		}
-		// -----
+  function wrappedExportedCode(stage) {
+    if (stage === 'preload') {
+      if (setup !== window.setup) {
+        window.setup = setup;
+      } else {
+        return;
+      }
+    }
 
-		// Draw a 40 x 40 grid on a piece of paper and start colouring your idea on it,
-		// This will help you code faster.
+    // -----
 
-		//  Your pixel grid has 40 rows and 40 columns.
-		//  Every time you change the code click on "Reset" then click "Run"
-		//  This will refresh your canvas.
+    // Mission: You are on the MISSION MARS. Make your Signature by creating trace using your ROVER  //
+
+    //Create a sprite for the background and adding image
+    var bg = createSprite(200, 200, 10, 10);
+    bg.setAnimation("bg1.png");
+
+    //Create a rover to make traces and adding image to the rover.
+    var rover = createSprite(200, 200, 10, 10);
+    rover.setAnimation("rover4.png");
+    rover.scale = 0.75;
 
 
-		var row_1 = "8888888888888880000000088888888888888888";
-		var row_2 = "8888888888888800000000008888888888888888";
-		var row_3 = "8888888888888000000000000888888888888888";
-		var row_4 = "8888888888880000000000000088888888888888";
-		var row_5 = "8888888888800000000000000008888888888888";
-		var row_6 = "8888888888800000000000000000888888888888";
-		var row_7 = "8888888888800000000000000000888888888888";
-		var row_8 = "8888888888800040000000000000888888888888";
-		var row_9 = "8888888888800044444444444400888888888888";
-		var row_10 = "8888888888880044004444400440888888888888";
-		var row_11 = "8888888888888040110444011040888888888888";
-		var row_12 = "8888888888884400110000011004488888888888";
-		var row_13 = "8888888888884440110444011044488888888888";
-		var row_14 = "8888888888888444004494400444888888688888";
-		var row_15 = "8883333888888844444444444448888886688888";
-		var row_16 = "8883333888888844449444944448888866888888";
-		var row_17 = "8888338888888884444999444488888666688888";
-		var row_18 = "8884434888888888844888448888888666688888";
-		var row_19 = "8844224888888888884444488888888866888988";
-		var row_20 = "8844224888888888888444888888888668888988";
-		var row_21 = "8844224888888881116646611188886688888988";
-		var row_22 = "8882224888888811111666111118868888888988";
-		var row_23 = "8882222188888111111666111111868888888988";
-		var row_24 = "8822222218881111111666111111333383333908";
-		var row_25 = "8822222211811111111161111111133888000000";
-		var row_26 = "8822222211111111111161111111143888338908";
-		var row_27 = "8888888111111811111111111181466488338988";
-		var row_28 = "8888888811118811111111111188466488558988";
-		var row_29 = "8833333881188811111133331188466488558988";
-		var row_30 = "8883338888888811111113331188866488558988";
-		var row_31 = "8885558888888811111117771188866888558988";
-		var row_32 = "8885558888888811111117771188888888888988";
-		var row_33 = "8885558888888811111117771188888999999998";
-		var row_34 = "4444444444444444444444444444444444444444";
-		var row_35 = "4114411444441144111411141414141411141414";
-		var row_36 = "4141414144441414144414441414141414441414";
-		var row_37 = "4141411444441144114411444144141441441114";
-		var row_38 = "4141414144441444144414444144141444141414";
-		var row_39 = "4114414141441444111411144144111411141414";
-		var row_40 = "4444444444444444444444444444444444444444";
+    //other variables
+    var lines = [];
+    var timeCalled = 0;
+    var penDown = true;
 
-		// 0 ==> Black
-		// 1 ==> White
-		// 2 ==> Red
-		// 3 ==> Yellow
-		// 4 ==> Orange
-		// 5 ==> Green
-		// 6 ==> Blue
-		// 7 ==> Purple
-		// 8 ==> Pink
-		// 9 ==> Brown
+
+    //Adding color to the trace created using the rover 
+    stroke(rgb(61, 43, 31));
+    strokeWeight(3);
+
+
+
+    function draw() {
+      background("white");
+
+      drawSprites();
+
+      //To display the trace you created using your rover. 
+      for (var x = 0; x < lines.length; x++) {
+        line(lines[x][0], lines[x][1],
+          lines[x][2], lines[x][3]);
+      }
+
+      if (windowHeight < windowWidth) {
+        document.body.style.zoom = (windowHeight / 400) - 0.03
+      }
+      else {
+        document.body.style.zoom = (windowWidth / 400) - 0.03
+      }
+    }
+
+
+
+    //       *******     sample code for reference      ********//
+
+    // SYNTAX : functionname (input)
+    // forward function moves front with the distance given as input. eg: forward(20) will move front by 20 steps.
+    // backward function moves backward with the distance given as input. eg: backward(20) will move backward by 20 steps.
+    // turnRight function rotates in clockwise with a certain degree . eg: turnRight(20) will rotate right side by 20 degrees.
+    // turnLeft function rotates in anti-clockwise with a certain degree . eg: turnLeft(20) will rotate left side by 20 degrees.
+    // lift up function will not make traces on the Mars.
+    //lift down will not make traces on the Mars.
 
 
 
 
+    //       *******     sample code for reference      ********//
+
+    // ROVER INSTRUCTIONS TO MARK YOUR TRACE -- START //
+
+    // enter your code here!
+
+
+
+    // Move rover to starting position
+    liftUp();
+    turnRight(90);
+    forward(15);
+    liftDown();
+
+    // Create fingers
+    turnLeft(45);
+    forward(80);
+    // Create arc
+    turnRight(45);
+    for (var i = 0; i < 15; i++) {
+      turnRight(6);
+      forward(3);
+    }
+
+    // Other side fingers
+    turnRight(135);
+    liftUp();
+    forward(41);
+    turnRight(90);
+    forward(2);
+    liftDown();
+    turnLeft(45);
+
+    for (var i = 0; i < 15; i++) {
+      turnLeft(6);
+      forward(3);
+    }
+    turnLeft(45);
+    forward(80);
+    liftUp();
+    backward(80);
+    liftDown();
+    turnRight(45);
+
+    turnRight(90);
+    for (var i = 0; i < 15; i++) {
+      turnLeft(6);
+      forward(3);
+    }
+
+    // Bring back rover for folded fingers
+    turnLeft(45);
+    forward(10);
+    backward(10);
+    turnLeft(90);
+    liftUp();
+    forward(123);
+    liftDown();
+
+    turnRight(45);
+
+    // Create folded fingers
+    // First 3 folded fingers
+
+    for (var i = 0; i < 3; i++) {
+      foldedFinger();
+      turnRight(90);
+      liftUp();
+      forward(38);
+      liftDown();
+      turnLeft(45);
+    }
+
+    // Fourth folded finger
+    foldedFinger();
+    turnRight(90);
+    liftUp();
+    forward(38);
+    turnLeft(90);
+    forward(50);
+    liftDown();
+    turnRight(90);
+
+
+    // Creating heart
+    for (var i = 0; i < 22; i++) {
+      turnRight(2);
+      forward(5);
+    }
+    turnRight(1);
+
+    for (var i = 0; i < 40; i++) {
+      turnRight(3.5);
+      forward(5);
+    }
+
+    turnRight(260);
+
+    for (var i = 0; i < 40; i++) {
+      turnRight(3.5);
+      forward(5);
+    }
+    turnRight(1);
+    for (var i = 0; i < 12; i++) {
+      turnRight(3);
+      forward(6);
+    }
+
+    turnRight(3);
+    forward(2);
+
+    turnRight(95);
+    forward(120);
+
+    // Create Thumb
+    turnLeft(100);
+    for (var i = 0; i < 7; i++) {
+      turnRight(3);
+      forward(10);
+    }
+
+    // increasing curve of the thumb
+    for (var i = 1; i < 13; i++) {
+      turnRight(i);
+      forward(2);
+    }
+
+    for (var i = 1; i < 8; i++) {
+      turnRight(6);
+      forward(1);
+    }
+
+    turnRight(30);
+    forward(80);
+
+    turnRight(15);
+    forward(25);
+
+
+    // Bring rower to bottom
+    turnRight(140);
+    liftUp();
+    forward(285);
+    turnLeft(90);
+    forward(130);
+    liftDown();
+
+
+    //make folded finger
+    function foldedFinger() {
+      turnLeft(45);
+      for (var j = 0; j < 2; j++) {
+        forward(60);
+        for (var i = 0; i < 60; i++) {
+          turnRight(3);
+          forward(1);
+        }
+      }
+    }
 
 
 
 
+    // ROVER INSTRUCTIONS TO MARK YOUR TRACE -- END //
+
+
+    //Function to control the steps and directions of the rover. 
+    //rover.rotation is used for rotating the rover in a particular angle.
+    //x1 - start position in x direction
+    //y1- start position in y direction
+    //x2 -  position in x direction after travelling a certain distance
+    //y2 - position in y direction after travelling a certain distance
+    //one degree is equivalent to pi/180 radians. 
+    //Math.cos(),Math.sin() method returns a numeric value between -1 and 1.
+
+    function move(angle, distance) {
+      timeCalled += 1;
+      setTimeout(function () {
+        rover.rotation = rover.rotation + angle;
+        var radian = rover.rotation * Math.PI / 180;
+        var x1 = rover.x;
+        var y1 = rover.y;
+        var x2 = rover.x + (distance * Math.cos(radian));
+        var y2 = rover.y + (distance * Math.sin(radian));
+        rover.x = x2;
+        rover.y = y2;
+        if (penDown === true) {
+          lines.push([x1, y1, x2, y2]);
+        }
+      }, timeCalled * 250);
+    }
+
+    // To move the rove in forward direction
+    function forward(distance) {
+      move(0, distance);
+    }
+
+    //To move the rover in backward riection
+    function backward(distance) {
+      move(0, -distance);
+    }
+
+    // To rotate the rover in clockwise directiopn
+    function turnRight(angle) {
+      move(angle, 0);
+    }
+
+    // To move the rover in anticlockwise direction
+    function turnLeft(angle) {
+      move(-angle, 0);
+    }
+
+    // Lift the rover up to avoid making traces on the ground
+    function liftUp() {
+      timeCalled += 1;
+      setTimeout(function () {
+        penDown = false;
+      }, 250 * timeCalled);
+    }
+
+    // Make the rover touch the ground to draw traces on the ground
+    function liftDown() {
+      timeCalled += 1;
+      setTimeout(function () {
+        penDown = true;
+      }, 250 * timeCalled);
+    }
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-		var rows = [row_1, row_2, row_3, row_4, row_5, row_6, row_7, row_8,
-			row_9, row_10, row_11, row_12, row_13, row_14, row_15, row_16, row_17,
-			row_18, row_19, row_20, row_21, row_22, row_23, row_24, row_25, row_26,
-			row_27, row_28, row_29, row_30, row_31, row_32, row_33, row_34, row_35,
-			row_36, row_37, row_38, row_39, row_40];
-
-
-		background("white");
-
-		for (var k = 0; k <= 40; k++) {
-			line(k * 10, 0, k * 10, 400);
-			line(0, k * 10, 400, k * 10);
-		}
-
-		// rows
-		for (var r = 0; r < rows.length; r++) {
-			for (var i = 0; i < rows[r].length; i++) {
-				if (rows[r][i] == "0") {
-					// console.log("entered" + i);
-					strokeWeight(0.02)
-					fill("black");
-					rect(i * 10, r * 10, 10, 10);
-				}
-				else if (rows[r][i] == "1") {
-					strokeWeight(0.02)
-					fill("white");
-					rect(i * 10, r * 10, 10, 10);
-				}
-				else if (rows[r][i] == "2") {
-					strokeWeight(0.02)
-					fill("red");
-					rect(i * 10, r * 10, 10, 10);
-				}
-				else if (rows[r][i] == "3") {
-					strokeWeight(0.02)
-					fill("yellow");
-					rect(i * 10, r * 10, 10, 10);
-				}
-				else if (rows[r][i] == "4") {
-					strokeWeight(0.02)
-					fill("orange");
-					rect(i * 10, r * 10, 10, 10);
-				}
-				else if (rows[r][i] == "5") {
-					strokeWeight(0.02)
-					fill("green");
-					rect(i * 10, r * 10, 10, 10);
-				}
-				else if (rows[r][i] == "6") {
-					strokeWeight(0.02)
-					fill("blue");
-					rect(i * 10, r * 10, 10, 10);
-				}
-				else if (rows[r][i] == "7") {
-					strokeWeight(0.02)
-					fill("purple");
-					rect(i * 10, r * 10, 10, 10);
-				}
-				else if (rows[r][i] == "8") {
-					strokeWeight(0.02)
-					fill("pink");
-					rect(i * 10, r * 10, 10, 10);
-				}
-				else if (rows[r][i] == "9") {
-					strokeWeight(0.02)
-					fill("brown");
-					rect(i * 10, r * 10, 10, 10);
-				}
-			}
-		}
-		drawSprites();
-
-		// -----
-		try { window.draw = draw; } catch (e) { }
-		switch (stage) {
-			case 'preload':
-				if (preload !== window.preload) { preload(); }
-				break;
-			case 'setup':
-				if (setup !== window.setup) { setup(); }
-				break;
-		}
-	}
-	window.wrappedExportedCode = wrappedExportedCode;
-	wrappedExportedCode('preload');
+    // -----
+    try { window.draw = draw; } catch (e) { }
+    switch (stage) {
+      case 'preload':
+        if (preload !== window.preload) { preload(); }
+        break;
+      case 'setup':
+        if (setup !== window.setup) { setup(); }
+        break;
+    }
+  }
+  window.wrappedExportedCode = wrappedExportedCode;
+  wrappedExportedCode('preload');
 };
 
 window.setup = function () {
-	window.wrappedExportedCode('setup');
+  window.wrappedExportedCode('setup');
 };
-
-function draw() {
-	if (windowHeight < windowWidth) {
-		document.body.style.zoom = (windowHeight / 400) - 0.03
-	}
-	else {
-		document.body.style.zoom = (windowWidth / 400) - 0.03
-	}
-}
